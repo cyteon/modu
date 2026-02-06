2693eb1cea155d631edb46156885c301ed22275e
cyteon <me@cyteon.dev>
2026-02-06 22:27:23 +0100

    fix file stuff and remove tests for it cause yea

--- lang/src/libraries/file.rs
+++ lang/src/libraries/file.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::fs::OpenOptions;
 use crate::{ast::{Expr, InternalFunctionResponse, Spanned, SpannedExpr}, lexer::Span};
 
@@ -87,14 +87,24 @@ pub fn write(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (Stri
             args[1].span,
         )),
     };
+    
+    (&**file).seek(SeekFrom::Start(0)).map_err(|e| (
+        format!("Failed to seek to beginning of file: {}", e),
+        args[0].span,
+    ))?;
 
-    std::io::Write::write_all(&mut &**file, content.as_bytes()).map_err(|e| (
+    (&**file).write_all(content.as_bytes()).map_err(|e| (
         format!("Failed to write to file: {}", e),
         args[0].span,
     ))?;
 
     file.set_len(content.len() as u64).map_err(|e| (
         format!("Failed to truncate file: {}", e),
+        args[0].span,
+    ))?;
+
+    (&**file).flush().map_err(|e| (
+        format!("Failed to flush file: {}", e),
         args[0].span,
     ))?;
 
@@ -120,15 +130,45 @@ pub fn append(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (Str
             args[1].span,
         )),
     };
+    
+    (&**file).write_all(content.as_bytes()).map_err(|e| (
+        format!("Failed to write to file: {}", e),
+        args[0].span,
+    ))?;
 
-    std::io::Write::write_all(&mut std::io::BufWriter::new(&**file), content.as_bytes()).map_err(|e| (
-        format!("Failed to append to file: {}", e),
+    (&**file).flush().map_err(|e| (
+        format!("Failed to flush file: {}", e),
         args[0].span,
     ))?;
 
     Ok(InternalFunctionResponse {
         return_value: Expr::Null,
         replace_self: None,
+    })
+}
+
+pub fn close(args: Vec<Spanned<Expr>>) -> Result<InternalFunctionResponse, (String, Span)> {
+    let file = match &args[0].node {
+        Expr::File(file) => file.clone(),
+        _ => return Err((
+            "close expects a file argument".to_string(),
+            args[0].span,
+        )),
+    };
+    
+    std::io::Write::flush(&mut &*file).map_err(|e| (
+        format!("Failed to flush file: {}", e),
+        args[0].span,
+    ))?;
+
+    file.sync_all().map_err(|e| (
+        format!("Failed to sync file: {}", e),
+        args[0].span,
+    ))?;
+    
+    Ok(InternalFunctionResponse {
+        return_value: Expr::Null,
+        replace_self: Some(Expr::Null),
     })
 }
 
@@ -139,12 +179,14 @@ pub fn get_fn(name: &str) -> Option<Expr> {
             "read" => vec!["self".to_string()],
             "write" => vec!["self".to_string(), "content".to_string()],
             "append" => vec!["self".to_string(), "content".to_string()],
+            "close" => vec!["self".to_string()],
             _ => vec![],
         },
         func: match name {
             "read" => read,
             "write" => write,
             "append" => append,
+            "close" => close,
             _ => return None,
         },
     })
