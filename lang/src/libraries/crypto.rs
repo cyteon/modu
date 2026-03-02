34119616ace11b6ec945f80e76c6029c4131320d
Cyteon <cyteon@hackclub.app>
2026-03-02 14:16:24 +0100

    tests for crypto

--- lang/src/libraries/crypto.rs
+++ lang/src/libraries/crypto.rs
@@ -382,3 +382,48 @@ pub fn get_object() -> Expr {
 
     Expr::Module { symbols }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sha25() {
+        let result = sha256(vec![ SpannedExpr { node: Expr::String("test".to_string()), span: Span::default() } ]);
+
+        assert_eq!(
+            result.unwrap().return_value, 
+            Expr::String("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08".to_string())
+        )
+    }
+
+    #[test]
+    fn test_sha512() {
+        let result = sha512(vec![ SpannedExpr { node: Expr::String("test".to_string()), span: Span::default() } ]);
+
+        assert_eq!(
+            result.unwrap().return_value, 
+            Expr::String("ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff".to_string())
+        )
+    }
+
+    #[test]
+    fn test_blake3() {
+        let result = blake3(vec![ SpannedExpr { node: Expr::String("test".to_string()), span: Span::default() } ]);
+
+        assert_eq!(
+            result.unwrap().return_value, 
+            Expr::String("4878ca0425c739fa427f7eda20fe845f6b2e46ba5fe2a14df5b1e32f50603215".to_string())
+        )
+    }
+
+    #[test]
+    fn test_md5() {
+        let result = md5(vec![ SpannedExpr { node: Expr::String("test".to_string()), span: Span::default() } ]);
+
+        assert_eq!(
+            result.unwrap().return_value, 
+            Expr::String("098f6bcd4621d373cade4e832627b4f6".to_string())
+        )
+    }
+}
