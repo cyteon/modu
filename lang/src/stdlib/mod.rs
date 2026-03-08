18c39636e19ec47bb0383be887612c390bc7e892
cyteon <me@cyteon.dev>
2026-03-08 13:12:58 +0100

    os package and fix tests

--- lang/src/stdlib/mod.rs
+++ lang/src/stdlib/mod.rs
@@ -4,6 +4,9 @@ mod math;
 mod time;
 mod uuid;
 
+#[cfg(not(target_arch = "wasm32"))]
+mod os;
+
 pub fn get(name: &str) -> Option<crate::vm::value::Value> {
     match name {
         "crypto" => Some(crypto::object()),
@@ -11,6 +14,9 @@ pub fn get(name: &str) -> Option<crate::vm::value::Value> {
         "math" => Some(math::object()),
         "time" => Some(time::object()),
         "uuid" => Some(uuid::object()),
+
+        #[cfg(not(target_arch = "wasm32"))]
+        "os" => Some(os::object()),
         _ => None,
     }
 }
