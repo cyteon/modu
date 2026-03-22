7233eb64f7885d11a2166e0e123f890b43006c12
cyteon <me@cyteon.dev>
2026-03-22 11:32:23 +0100

    make a super simple grammar

--- extensions/zed/grammars/modu/grammar.js
+++ extensions/zed/grammars/modu/grammar.js
@@ -1,9 +1,15 @@
-const NEWLINE = /\r?\n/;
-
-
 module.exports = grammar({
     name: "modu",
     rules: {
+        program: ($) => repeat($._expression),
+
+        _expression: ($) =>
+            choice(
+                $.identifier,
+                $.number,
+                $.string,
+            ),
+
         identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
         number: ($) => /\d+(\.\d+)?/,
