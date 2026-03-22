2faef3b3c561961de2402f963841aabb70dd5877
cyteon <me@cyteon.dev>
2026-03-22 13:41:53 +0100

    highlighter works

--- extensions/zed/languages/modu/highlights.scm
+++ extensions/zed/languages/modu/highlights.scm
@@ -1,3 +1,67 @@
+[
+  "let"
+  "fn"
+  "return"
+  "if"
+  "else"
+  "for"
+  "while"
+  "loop"
+  "class"
+  "import"
+  "as"
+  "in"
+  "and"
+  "or"
+] @keyword
+
+(boolean) @constant.builtin
+(null) @constant.builtin
+(self) @variable.special
 (string) @string
-(escape_sequence) @string.escape
 (number) @number
+(comment) @comment
+
+(fn_stmt
+  name: (identifier) @function)
+
+(call_expr
+  function: (identifier) @function)
+
+; for <smth>.<smth>() calls
+(call_expr
+  function: (property_expr
+    property: (identifier) @function.method))
+
+(class_decl
+  name: (identifier) @type)
+
+(property_expr
+  property: (identifier) @property)
+ 
+(fn_stmt
+  (identifier) @variable.parameter)
+
+[
+  "+"
+  "-"
+  "*"
+  "/"
+  "%"
+  "**"
+  "=="
+  "!="
+  "<"
+  ">"
+  "<="
+  ">="
+  "="
+  "+="
+  "-="
+  "*="
+  "/="
+  "!"
+] @operator
+ 
+["(" ")" "[" "]" "{" "}"] @punctuation.bracket
+["," "." ":"] @punctuation.delimiter
